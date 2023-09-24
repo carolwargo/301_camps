d25db1659c90d4c21e151b422b95f967a7d6e876
Carol Wargo <Carolwargo@gmail.com>
2023-09-24 08:23:16 -0400

    updating nav and imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Home from './pages/Home';
-import Navbar from './components/navbar';
+import Navbar from './components/navbar/index.jsx';
 
 
 function App() {
