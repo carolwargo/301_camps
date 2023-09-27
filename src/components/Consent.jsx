4b1e60485308e156df5b089721cad9255dfeabee
Carol Wargo <Carolwargo@gmail.com>
2023-09-27 10:14:27 -0400

    udpate path

---
+++ src/components/Consent.jsx
@@ -0,0 +1,174 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+
+
+export default function Consent() {
+    return (
+
+<div className='consent' id='consent'>
+    <Container>
+        <br></br>
+    <h2>Terms & Conditions</h2>
+    <p>Welcome to 301CATCHING.COM ("we", "our", or "us"). By using our website, you agree to comply with and be bound by the following terms and conditions of use. Please review these terms carefully before using our website.</p>
+
+<p>By clicking the "Submit" button, you consent to the following:</p>
+<ul>
+<li>You agree to receive correspondence from us, which may include emails, newsletters, updates, and promotional materials.
+</li>
+<li>You understand that you can unsubscribe or opt out from receiving communications from us at any time by following the instructions provided in the emails.
+</li>
+<li>Your information will be handled in accordance with our Privacy Policy.
+</li>
+<li>You acknowledge that providing false or misleading information may result in the termination of your account or services.
+</li>
+<li>You confirm that you are at least 18 years old and have the legal capacity to enter into agreements.
+</li>
+</ul>
+<p>Please review our full Privacy Policy for more details on how we collect, use, and protect your information.</p>
+<h5>TERMS OF USE</h5>
+<ul>  
+<li>Acceptance of Terms: <br></br>
+By accessing and using this website, you agree to be bound by these terms of use and all applicable laws and regulations. If you do not agree with these terms, you are prohibited from using or accessing this site.
+</li>
+<li>
+GDPR Compliance:<br></br>
+We are committed to ensuring that your privacy is protected. Our privacy practices are in compliance with the General Data Protection Regulation (GDPR). For more information on how we handle and protect your personal data, please refer to our Privacy Policy.
+</li>
+<li>
+Intellectual Property:<br></br>
+All content on this website, including text, graphics, logos, images, and software, is the property of 301CATCHING and is protected by applicable copyright and trademark laws.
+</li>
+<li>
+User Conduct:<br></br>
+You agree to use this website for lawful purposes and in a way that does not infringe upon the rights of others. You are prohibited from engaging in any activity that may disrupt or interfere with the proper functioning of the website.
+</li>
+<li>
+Disclaimer of Warranties:<br></br>
+This website is provided "as is" without any warranties, express or implied. We do not warrant the accuracy, completeness, or reliability of any content or information on this site.
+</li>
+<li>
+Limitation of Liability:<br></br>
+We shall not be liable for any direct, indirect, incidental, special, or consequential damages arising out of the use or inability to use this website.
+</li>
+<li>
+Changes to Terms:<br></br>
+We reserve the right to revise these terms of use at any time without prior notice. By using this website, you are agreeing to be bound by the current version of these terms.
+</li>
+<li>
+Governing Law:<br></br>
+These terms and conditions are governed by and construed in accordance with the laws of the State of Maryland, and you irrevocably submit to the exclusive jurisdiction of the courts in that location.
+</li>
+<li>Cookie Policy:<br></br>
+This Cookie Policy explains what cookies are, how we use them, and your choices regarding cookies. Please read this policy to understand how we use cookies on our website.
+
+What are Cookies?
+Cookies are small text files that are stored on your device when you visit a website. They are widely used to make websites work more efficiently, as well as to provide information to the owners of the site.
+
+How We Use Cookies
+We use cookies for the following purposes:
+
+Essential Cookies: These cookies are necessary for the website to function properly. They enable basic functionalities like page navigation and access to secure areas of the website.
+
+Analytical/Performance Cookies: These cookies help us understand how visitors interact with our website by providing information about the areas visited, time spent on the site, and any issues encountered.
+
+Functional Cookies: These cookies allow the website to remember choices you make (such as language preferences) and provide enhanced features.
+
+Disabling Cookies
+You can prevent the setting of cookies by adjusting the settings on your browser. Be aware that disabling cookies may affect the functionality of this and many other websites you visit. Disabling cookies will usually result in also disabling certain functionality and features of this site.
+
+Third-Party Cookies
+In some special cases, we also use cookies provided by trusted third parties. The following section details which third-party cookies you might encounter through this site.
+
+More Information
+If you would like more information about cookies, please visit www.allaboutcookies.org.
+
+Changes to This Cookie Policy
+We may update this Cookie Policy from time to time. Any changes will be posted on this page.
+
+</li>
+</ul>
+<br></br>
+<h5>PRIVACY POLICY</h5>
+    <p>This Privacy Policy describes how 301CATCHING ("we", "our", or "us") collects, uses, and shares information about you and explains the choices you have regarding your information.</p>
+    
+<ul>Information We Collect <br></br>
+We may collect the following types of information:
+<ul>
+<li>Personal Information:<br></br>This may include your name, email address, postal address, phone number, and other personally identifiable information you provide to us.
+</li>
+<li>Usage Information:<br></br> We may collect information about how you use our website or services, such as the pages you visit, the links you click, and the actions you take.
+</li>
+<li>Device Information:<br></br> We may collect information about the device you use to access our website or services, such as the device type, operating system, and browser.
+</li>
+<li>Log Information: <br></br> When you use our website, our servers may automatically record information, including your IP address, browser type, and referral source.
+</li>
+</ul>
+</ul>
+<ul>
+How We Use Your Information <br></br>
+We may use your information for the following purposes:
+<ul>
+<li>To provide and maintain our services.</li>
+<li>To improve, personalize, and customize our services.</li>
+<li>To communicate with you, including responding to your inquiries and sending you updates.</li>
+<li>To analyze usage and performance of our website and services.</li>
+<li>To comply with legal and regulatory requirements.</li>
+<li>Sharing Your Information
+We do not sell, trade, or otherwise transfer your personally identifiable information to outside parties. We may share your information with trusted third parties who assist us in operating our website, conducting our business, or servicing you.</li>
+</ul>
+</ul>
+Your Choices
+You may choose to restrict the collection or use of your personal information. You can do so by not providing personal information on our website or by adjusting your browser settings to restrict the use of cookies.
+
+Security
+We implement a variety of security measures to maintain the safety of your personal information.
+
+Changes to This Privacy Policy
+We may update this Privacy Policy from time to time. Any changes will be posted on this page.
+<br></br>
+<br></br>
+<div className='text-messaging' id='text-messaging'>
+<h5>TEXT-MESSAGING</h5>
+Consent to Receive Text Messages-
+
+By providing your mobile phone number and opting in to receive text messages, you are consenting to receive non-marketing and marketing text messages from 301CATCHING & its affliates, at the number provided. Message and data rates may apply. Types of Messages
+You may receive the following types of text messages:
+<ul>
+<li>Transaction Notifications: Updates and notifications related to your account or transactions.
+</li>
+<li>
+Service Alerts: Information about service interruptions, maintenance, or other important updates.
+</li>
+<li>
+Marketing Messages: Promotional offers, discounts, and information about our products or services.
+
+Opting Out
+
+You can opt out of receiving text messages at any time by replying "STOP" to any message you receive. This will unsubscribe you from all text message communications.
+</li>
+<li>
+Data Usage and Privacy
+
+We respect your privacy. Your mobile number will only be used for the purposes of sending text messages related to [Your Company Name]. We will not share or sell your mobile number to third parties.
+</li>
+<li>
+Help and Support
+
+For help or support related to text messages, please contact our customer support team at <a href="mailto:301catching@gmail.com">301CATCHING@gmail.com</a> .
+</li>
+</ul>
+</div>
+
+<br></br>
+<h4>CONTACT US</h4>
+If you have any questions or concerns about these terms of use, please contact us at <a href="mailto:301catching@gmail.com">
+          301catching@GMAIL.COM
+        </a>{" "}.
+
+</Container>
+
+</div>
+    );
+}
---
+++ src/pages/Consent/TextConsent.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+
+
+
+
+export default function Consent() {
+    return (
+
+<div className='text-messaging' id='text-messaging'>
+    <Container>
+<br></br>
+<br></br>
+
+<h5>TEXT-MESSAGING</h5>
+Consent to Receive Text Messages-
+
+By providing your mobile phone number and opting in to receive text messages, you are consenting to receive non-marketing and marketing text messages from 301CATCHING & its affliates, at the number provided. Message and data rates may apply. Types of Messages
+You may receive the following types of text messages:
+<ul>
+<li>Transaction Notifications: Updates and notifications related to your account or transactions.
+</li>
+<li>
+Service Alerts: Information about service interruptions, maintenance, or other important updates.
+</li>
+<li>
+Marketing Messages: Promotional offers, discounts, and information about our products or services.
+
+Opting Out
+
+You can opt out of receiving text messages at any time by replying "STOP" to any message you receive. This will unsubscribe you from all text message communications.
+</li>
+<li>
+Data Usage and Privacy
+
+We respect your privacy. Your mobile number will only be used for the purposes of sending text messages related to [Your Company Name]. We will not share or sell your mobile number to third parties.
+</li>
+<li>
+Help and Support
+
+For help or support related to text messages, please contact our customer support team at <a href="mailto:301catching@gmail.com">301CATCHING@gmail.com</a> .
+</li>
+</ul>
+
+
+<br></br>
+<h4>CONTACT US</h4>
+If you have any questions or concerns about these terms of use, please contact us at <a href="mailto:301catching@gmail.com">
+          301catching@GMAIL.COM
+        </a>{" "}.
+
+</Container>
+
+</div>
+    );
+}
